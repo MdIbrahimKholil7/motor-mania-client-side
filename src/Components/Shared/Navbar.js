c53fb430117067a9028a27c82a538778e9603dda
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-05-22 01:03:34 +0600

    navbar complete

---
+++ src/Components/Shared/Navbar.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import TopBar from './TopBar';
+
+const Navbar = () => {
+    const menu = [
+        { name: 'Home', to: '/' },
+        { name: 'Dashboard', to: '/dashboard' },
+        { name: 'Blog', to: '/blog' },
+        { name: 'About', to: '/about' },
+    ]
+    return (
+        <nav>
+            <TopBar/>
+            <div class="drawer">
+                <input id="my-drawer-3" type="checkbox" class="drawer-toggle" />
+                <div class="drawer-content flex flex-col">
+                    {/* Navbar  */}
+                    <div class="w-full navbar">
+                        <div class="flex-1 text-4xl font-bold font">Parts<span className='text-primary '>Mania</span></div>
+                        <div class="flex-none lg:hidden">
+                            <label for="my-drawer-3" class="btn btn-square btn-ghost">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-6 h-6 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                            </label>
+                        </div>
+
+                        <div class="flex-none hidden lg:block ">
+                            <ul class="menu menu-horizontal items-center gap-x-3">
+                                {/* Navbar menu content here  */}
+                                {
+                                    menu.map(({ name, to }, index) => <NavLink
+                                        key={index}
+                                        to={to}>
+                                        <li className='font-bold lg:text-[18px] xl:text-2xl'>
+                                            {name}
+                                        </li>
+                                    </NavLink>
+                                    )
+                                }
+                                <li>
+                                    <div class="dropdown dropdown-end dropdown-hover">
+                                        <label className='font-bold relative lg:text-[18px] xl:text-2xl shop' tabindex="0">Shop</label>
+                                        <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52 mt-36">
+                                            <li><a>Item 1</a></li>
+                                            <li><a>Item 2</a></li>
+                                        </ul>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    {/* Page content here 
+                    Content */}
+                </div>
+                <div class="drawer-side">
+                    <label for="my-drawer-3" class="drawer-overlay"></label>
+                    <ul class="menu p-4 overflow-y-auto w-80 bg-base-100">
+                        {/* Sidebar content here  */}
+                        {
+                            menu.map(({ name, to }, index) => <NavLink
+                                key={index}
+                                to={to}>
+                                <li>
+                                    {name}
+                                </li>
+                            </NavLink>
+                            )
+                        }
+
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
---
+++ src/Components/Shared/TopBar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { SearchIcon, ShoppingCartIcon, UserIcon } from '@heroicons/react/solid'
+const TopBar = () => {
+    return (
+        <div >
+            <div class=" w-full px-4">
+                <div class=" pt-4 card  rounded-box flex flex-row justify-evenly items-center">
+                    <div class="form-control">
+                        <label class="input-group ">
+                            <input type="text" placeholder="Search" class="input input-bordered focus:outline-none" />
+                            <span className='bg-primary cursor-pointer text-white'>
+                                <SearchIcon
+                                    className='w-5 h-5 '
+                                />
+                            </span>
+                        </label>
+                    </div>
+                    <div className=''>
+                        <div class="avatar placeholder items-center">
+                            <div class="bg-primary text-white rounded-full w-10">
+                                <span className=''>
+                                    <UserIcon
+                                        className='w-5 h-5'
+                                    />
+                                </span>
+                            </div>
+                            <h1 className='ml-2 font-bold font text-[18px]'>My Profile</h1>
+                        </div>
+                        <div class="avatar placeholder items-center ml-9">
+                            <div class="bg-primary text-white rounded-full w-10">
+                                <span className=''>
+                                    <ShoppingCartIcon
+                                        className='w-5 h-5'
+                                    />
+                                </span>
+                            </div>
+                            <h1 className='ml-2 font-bold font text-[18px]'>Cart</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="divider m-0 pt-3"></div>
+        </div>
+    );
+};
+
+export default TopBar;
