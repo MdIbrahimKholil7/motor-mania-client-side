1bb45206c213baebe733e290e0c4a8409206636a
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-05-27 04:14:27 +0600

    user order data get api complete

--- src/Components/PublicRoutes/MangeAllOrder.js
+++ src/Components/PublicRoutes/MangeAllOrder.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import axiosPrivate from '../../api/axiosPrivate';
 
 const MangeAllOrder = () => {
+    const { loading, data, refetch } = useQuery('get-all-paid', () => axiosPrivate.get('http://localhost:5000/get-all-users-order'))
+    console.log(data)
+    if (loading) {
+        return loading
+    }
     return (
         <div>
-            <h1>This is Mangae All order</h1>
+            
         </div>
     );
 };
