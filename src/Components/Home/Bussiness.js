61dd662f8e62e285ad741c3573b362771a3ad1dc
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-07-24 22:47:15 +0600

    counter complete

--- src/Components/Home/Bussiness.js
+++ src/Components/Home/Bussiness.js
@@ -1,20 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FaFlag, FaUserFriends, FaCompass, FaRegBookmark } from 'react-icons/fa';
-// import CountUp, { useCountUp } from 'react-countup';
+import CountUp, { useCountUp } from 'react-countup';
+import VisibilitySensor from 'react-visibility-sensor';
 const Bussiness = () => {
-    /*  const {countUp,start,}=useCountUp({
-         duration:3,
-         end:300
-     })
-      */
-  /*   useEffect(() => {
-        window.addEventListener('scroll', (e) => {
-            console.log(this.scrollY)
-            if (window.scrollY > 300) {
-                console.log(window.pageYOffset == window.scrollY)
-            }
-        })
-    }, []) */
+   const [focus, setFocus] = useState(false)
     return (
         <div>
            
@@ -25,7 +14,22 @@ const Bussiness = () => {
                     />
 
                     <div className="stat-title">Countries</div>
-                    <div className="stat-value">20+</div>
+
+                    <CountUp start={focus ? 0 : null} end={20} duration={3}>
+                        {({ countUpRef }) => (
+                            <VisibilitySensor onChange={isVisible => {
+                                if (isVisible) {
+                                    setFocus(true)
+                                }
+                            }}>
+                                <div>
+                                    <span className="stat-value" ref={countUpRef} /><span className="stat-value">K</span>
+                                   
+                                </div>
+                            </VisibilitySensor>
+                        )}
+                    </CountUp>
+          
 
                 </div>
 
@@ -34,7 +38,21 @@ const Bussiness = () => {
                         className='text-3xl text-center'
                     />
                     <div className="stat-title">Order Complete</div>
-                    <div className="stat-value">4,200+</div>
+                    <CountUp start={focus ? 0 : null} end={4200} decimal={1} duration={3}>
+                        {({ countUpRef }) => (
+                            <VisibilitySensor onChange={isVisible => {
+                                if (isVisible) {
+                                    setFocus(true)
+                                }
+                            }}>
+                                <div>
+                                    <span className="stat-value" ref={countUpRef} /><span className="stat-value">K</span>
+                                   
+                                </div>
+                            </VisibilitySensor>
+                        )}
+                    </CountUp>
+                    {/* <div className="stat-value">4,200+</div> */}
 
                 </div>
 
@@ -43,7 +61,21 @@ const Bussiness = () => {
                         className='text-3xl text-center'
                     />
                     <div className="stat-title">Happy Client</div>
-                    <div className="stat-value">1000K +</div>
+                    <CountUp start={focus ? 0 : null} end={1000} duration={3}>
+                        {({ countUpRef }) => (
+                            <VisibilitySensor onChange={isVisible => {
+                                if (isVisible) {
+                                    setFocus(true)
+                                }
+                            }}>
+                                <div>
+                                    <span className="stat-value" ref={countUpRef} /><span className="stat-value">K+</span>
+                                   
+                                </div>
+                            </VisibilitySensor>
+                        )}
+                    </CountUp>
+                    {/* <div className="stat-value">1000K +</div> */}
 
                 </div>
                 <div className="stat pl-20 md:pl-0 items-center">
@@ -51,7 +83,21 @@ const Bussiness = () => {
                         className='text-3xl text-center'
                     />
                     <div className="stat-title">Feedbacks</div>
-                    <div className="stat-value">3000K +</div>
+                    <CountUp start={focus ? 0 : null} end={3000} duration={3}>
+                        {({ countUpRef }) => (
+                            <VisibilitySensor onChange={isVisible => {
+                                if (isVisible) {
+                                    setFocus(true)
+                                }
+                            }}>
+                                <div>
+                                    <span className="stat-value" ref={countUpRef} /><span className="stat-value">K+</span>
+                                   
+                                </div>
+                            </VisibilitySensor>
+                        )}
+                    </CountUp>
+                    {/* <div className="stat-value">3000K +</div> */}
 
                 </div>
 
