9fd77d56c96fe00a433c97e5d0d36e5027c2ddff
MdIbrahimKholil7 <kholilmdibrahim17@gmail.com>
2022-07-27 02:31:50 +0600

    margin add

--- src/Components/Shared/Navbar.js
+++ src/Components/Shared/Navbar.js
@@ -37,7 +37,7 @@ const Navbar = ({ children }) => {
 
                                     <div className="flex-1 text-4xl font-bold font">Parts<span className='text-primary '>Mania</span></div>
                                     <div className="flex-none lg:hidden">
-                                        <label for="my-drawer-3" className="btn btn-square btn-ghost m-0 border-0">
+                                        <label for="my-drawer-3" className="btn btn-square btn-ghost  m-0 border-0">
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
                                         </label>
                                     </div>
